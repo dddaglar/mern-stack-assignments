e679286cfad3446d6a730a03643084a964832f71
Deniz <dddaglar54@gmail.com>
2023-06-17 22:51:16 +0300

    first react app

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+
+const Todo = () => {
+  return (
+    <div>
+      <h2>Deniz Dağlar</h2>
+      <p>I do web dev and digital marketing. <br /> In my free time, I like to tinker with all things tech.</p>
+      <ul>
+        <li>Portfolio</li>
+        <li>Meditations</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Todo;
